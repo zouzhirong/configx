dedd40899b8bdddd5afbcc900e9d727bca3d799b
zouzhirong <zouzhir@163.com>
2017-09-01 22:34:14 +0800

    修正@EnableMessageSource，在afterPropertiesSet方法中无法获取到国际化消息的问题

--- configx-client/src/main/java/com/configx/client/messagesource/ConfigMessageSourceConfigurer.java
+++ configx-client/src/main/java/com/configx/client/messagesource/ConfigMessageSourceConfigurer.java
@@ -4,14 +4,13 @@
  */
 package com.configx.client.messagesource;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.EnvironmentAware;
-import org.springframework.context.HierarchicalMessageSource;
-import org.springframework.context.MessageSource;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.*;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
@@ -22,17 +21,25 @@ import org.springframework.core.env.Environment;
  * @author Zhirong Zou
  */
 
-public class ConfigMessageSourceConfigurer implements
+public class ConfigMessageSourceConfigurer implements BeanDefinitionRegistryPostProcessor,
         BeanFactoryAware,
-        EnvironmentAware,
-        ApplicationListener<ContextRefreshedEvent> {
+        ApplicationContextAware,
+        EnvironmentAware {
 
     private ConfigurableListableBeanFactory beanFactory;
 
+    private ApplicationContext applicationContext;
+
     private ConfigurableEnvironment environment;
+
 
     public void setBeanFactory(BeanFactory beanFactory) {
         this.beanFactory = (ConfigurableListableBeanFactory) beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
     @Override
@@ -41,12 +48,12 @@ public class ConfigMessageSourceConfigurer implements
     }
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
         // MessageSource bean name
         String messageSourceBeanName = AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME;
 
         // 配置服务的MessageSource
-        HierarchicalMessageSource configMessageSource = new ConfigMessageSource(event.getApplicationContext());
+        HierarchicalMessageSource configMessageSource = new ConfigMessageSource(applicationContext);
 
         // 如果已经存在MessageSource bean,则将ConfigMessageSource设置为messageSource的parent,将messageSource原来的parent设置为ConfigMessageSource的parent
         if (beanFactory.containsLocalBean(messageSourceBeanName)) {
@@ -61,6 +68,10 @@ public class ConfigMessageSourceConfigurer implements
         } else {
             beanFactory.registerSingleton(messageSourceBeanName, configMessageSource);
         }
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
 
     }
 }
